70966008158a3bad2387688eaf3af807a26f37dc
satyam <satyamonsocial@gmail.com>
2021-12-22 13:57:36 +0530

    add client routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import './App.css';
+
+import Navbar from './components/Navbar';
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />}/> 
+          <Route exact path="/signup" element={<SignUp />}/>
+          <Route exact path="/profile" element={<Profile />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }

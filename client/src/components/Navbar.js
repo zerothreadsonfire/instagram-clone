8c9212dcdfd06cf1309241630e816eadebe3d616
satyam <satyamonsocial@gmail.com>
2022-01-03 21:54:37 +0530

    implement logout

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { userContext } from "../App";
 
 const Navbar = () => {
   const {state, dispatch} = React.useContext(userContext);
 
+  const navigate = useNavigate();
+
   const renderList = () => {
     if(state) {
       return [
         <li key="1"><Link to="/profile">Profile</Link></li>,
-        <li key="2"><Link to="/create">Create Post</Link></li>
+        <li key="2"><Link to="/create">Create Post</Link></li>,
+        <li>
+          <button className="btn red darken-1 my-2" onClick={() => {
+            localStorage.clear();
+            dispatch({type: "CLEAR"});
+            navigate("/login");
+          }}>Logout</button>
+        </li>
       ]
     } else {
       return [
--- client/src/reducers/userReducer.js
+++ client/src/reducers/userReducer.js
@@ -3,5 +3,7 @@ export const initialState = null;
 export const userReducer = (state, action) => {
   if(action.type === "USER") 
     return action.payload
+  else if(action.type === "CLEAR")
+    return null
   return state;
 }

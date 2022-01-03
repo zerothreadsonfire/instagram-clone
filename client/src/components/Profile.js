32be26ac17be0300f749625cd49491a78cb8f484
satyam <satyamonsocial@gmail.com>
2022-01-03 22:31:03 +0530

    implement profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,4 +1,21 @@
+import React from "react";
+import { userContext } from "../App";
+
 const Profile = () => {
+  const [posts, setPosts] = React.useState([]);
+  const {state, dispatch} = React.useContext(userContext);
+
+  React.useEffect(() => {
+    fetch("/myposts", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer "+localStorage.getItem("jwt")
+      }
+    }).then(res => res.json())
+    .then(res => setPosts(res.posts))
+  }, []);
+
   return(
     <div style={{maxWidth: "550px", margin: "0px auto"}}>
       <div style={{display:"flex", justifyContent:"space-around", margin:"18px 0", borderBottom: "1px solid grey"}}>
@@ -6,7 +23,7 @@ const Profile = () => {
           <img style={{width: "160px", height: "160px", borderRadius: "80px"}} alt="" src="https://images.unsplash.com/photo-1640103998727-2351128b0fa1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"></img>
         </div>
         <div>
-          <h4>Ramesh Verma</h4>
+          <h4>{state.name}</h4>
           <div style={{display: "flex", justifyContent: "space-between", width:"108%"}}>
             <h6>40 posts</h6>
             <h6>40 followers</h6>
@@ -16,12 +33,13 @@ const Profile = () => {
       </div>
 
       <div className="gallery">
-        <img className="item" alt="" src="https://images.pexels.com/photos/10255277/pexels-photo-10255277.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img>
-        <img className="item" alt="" src="https://images.pexels.com/photos/10255277/pexels-photo-10255277.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img>
-        <img className="item" alt="" src="https://images.pexels.com/photos/10255277/pexels-photo-10255277.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img>
-        <img className="item" alt="" src="https://images.pexels.com/photos/10255277/pexels-photo-10255277.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img>
-        <img className="item" alt="" src="https://images.pexels.com/photos/10255277/pexels-photo-10255277.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img>
-        <img className="item" alt="" src="https://images.pexels.com/photos/10255277/pexels-photo-10255277.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"></img>
+        {
+          posts.map( post => {
+            return (
+              <img className="item" alt={post.title} src={post.photo} key={post._id}></img>
+            )
+          })
+        }
       </div>
     </div>
   )
